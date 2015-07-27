179a3643017a2d9145d6dcd95d3336b56ff332ba
sh00x <sh00x.dev@gmail.com>
2015-07-27 20:19:45 +0200

    Main edited

--- src/com/company/OwnDialogFrame.java
+++ src/com/company/OwnDialogFrame.java
@@ -12,7 +12,7 @@ public class OwnDialogFrame extends JDialog {
 
     public OwnDialogFrame(JFrame owner) {
         super(owner, "Test okna O programie", true);
-        add(new JLabel("<html><h1><center><i>Informacje o programie</i></center></h1>", JLabel.CENTER), BorderLayout.NORTH);
+        add(new JLabel("<html><h1><center><b><u>Informacje o programie</u></b></center></h1>", JLabel.CENTER), BorderLayout.NORTH);
         add(new JLabel("That code smells. But window is modal ^_^", JLabel.CENTER), BorderLayout.CENTER);
 
         JPanel panel = new JPanel();
@@ -29,5 +29,6 @@ public class OwnDialogFrame extends JDialog {
         add(panel, BorderLayout.SOUTH);
 
         setSize(350, 150);
+        setResizable(false);
     }
 }
