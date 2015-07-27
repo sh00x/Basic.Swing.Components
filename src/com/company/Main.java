295f6d241ca796cee66eb651c45fffa430ef1489
sh00x <sh00x.dev@gmail.com>
2015-07-27 15:25:04 +0200

    Println in Main deleted

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("GITHUB TEST");
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
